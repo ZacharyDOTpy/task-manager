1fc9866b75ed7d1dc545cdc54551dfe3bf798d29
Sarahbryant411 <sarahbryant411@gmail.com>
2024-03-28 20:28:13 -0400

    added index.js in models

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,33 @@
-const User = require('./User');
+const User = require("./User");
+const Task = require("./Task");
+const TaskList = require("./TaskList");
 
-module.exports = { User };
+User.hasMany(Task, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Task.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+TaskList.hasMany(Task, {
+  foreignKey: "task_list_id",
+  onDelete: "CASCADE",
+});
+
+Task.belongsTo(TaskList, {
+  foreignKey: "task_list_id",
+});
+
+TaskList.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(TaskList, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+module.exports = { User, Task, TaskList };
+
