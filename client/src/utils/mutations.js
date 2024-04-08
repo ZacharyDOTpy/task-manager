f06972539dd1854f0739d640dd9a935b1d9a4f6c
Zach <zach.Brewer.1995@gmail.com>
2024-04-08 19:33:26 -0400

    added quireis and mutations for the front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,46 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const ADD_TASK = gql`
+  mutation addTask($name: String!, $description: String!) {
+    addTask(name: $name, description: $description) {
+      _id
+      name
+      description
+    }
+  }
+`;
+
+export const UPDATE_TASK = gql`
+  mutation updateTask($id: ID!, $name: String!, $description: String!) {
+    updateTask(id: $id, name: $name, description: $description) {
+      _id
+      name
+      description
+    }
+  }
+`;
+
+export const DELETE_TASK = gql`
+  mutation deleteTask($id: ID!) {
+    deleteTask(id: $id) {
+      _id
+    }
+  }
+`;
+
+export const ADD_USER_TASK = gql`
+  mutation addUserTask($taskId: ID!) {
+    addUserTask(taskId: $taskId) {
+      _id
+      username
+      tasks {
+        _id
+        name
+        description
+      }
+    }
+  }
+`;
+
